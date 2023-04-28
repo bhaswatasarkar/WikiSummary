b086a56ded5b26b9ca3f049a40ee4bd768b39233
Bhaswata Sarkar <bhaswatasarkar1@gmail.com>
2023-04-28 18:49:32 +0530

    updates

--- WikiSummary/settings.py
+++ WikiSummary/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-pzjj*^y*c4x##kmthhhggax^qz90oz%ow)v!)s4^v%@4+=_+p5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -113,6 +113,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
